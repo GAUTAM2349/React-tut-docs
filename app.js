3cceb172ae296722813c375a92dbfa5f2fdb3fd6
Gautam <gautamguddu577@gmail.com>
2024-11-17 18:46:34 +0530

    next we are going to use jsx..normal create element is mundane

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-/* note :-- 'react' is not any path yet. It is actually present inside node modules. so
-path would not be this above. But we are able to render, this is because bundlers (parcel in our case)
-handles these path related things, they know where we are trying to point */
-
-/* their were some changes in upgraded version of react-dom and they made package name to 'react-dom/client'. so 
-this might be needed somewhere */
 
 
-const h1 = React.createElement("h1", {}, "First Heading");
-const h2 = React.createElement("h2", {}, "Second Heading");
+const h1 = React.createElement("h1",
+   {
+     id: "first-heading",
+     className: "first-heading"
+   }, 
+   "This is the First Heading"
+  );
+
+const h2 = React.createElement("h2",
+   {
+     id: "second-heading",
+     className: "second-heading"
+   }, 
+   "This is the Second Heading"
+  );
+
+  const h3 = React.createElement("h3",
+    {
+      id: "third-heading",
+      className: "third-heading"
+    }, 
+    "This is the third Heading"
+   );
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const container = React.createElement(
+
+
+const heading = React.createElement(
   "div",
   {
     id: "container",
-    hellow: "gautam", //valid bcz eventually it is an js object only
+    hellow: "gautam", 
   },
-  [h1, h2]
+  [h1, h2,h3]
 );
 
-root.render(container);
+root.render(heading);
 
-// until this point we were doing everything wihtout NPM and parcel, but next we are going to use npm because
-// we will be installing many packages in future and managing them manually is cumbersome. Also we are going to
-// use parcel.
+ 
